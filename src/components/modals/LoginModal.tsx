02b4edbaa15477ca37d8ca8e58d028da95128575
Merdi-kim <merdikimuha1@gmail.com>
2023-06-27 16:33:07 +0300

    remove react from imports

--- src/components/modals/LoginModal.tsx
+++ src/components/modals/LoginModal.tsx
@@ -1,4 +1,4 @@
-import React, { Dispatch, SetStateAction } from 'react'
+import { Dispatch, SetStateAction } from 'react'
 import LensProfile from '../ui/LensProfile'
 import Image from 'next/image'
 
