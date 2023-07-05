f9d5808f434db68018255ddd901ac77a54305bc9
Merdi-kim <merdikimuha1@gmail.com>
2023-07-05 10:04:53 +0300

    adjust amount to fund to bundlr node

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -12,7 +12,7 @@ export async function POST(req: NextRequest) {
 			providerUrl: 'https://rpc-mumbai.maticvigil.com',
 		}
 	);
-	//const response = await bundlr.fund(bundlr.utils.toAtomic(0.4));
+	//const response = await bundlr.fund(bundlr.utils.toAtomic(0.5));
 	const body = await req.arrayBuffer();
 	const buffer = Buffer.from(body);
 	const tags = [{ name: 'Content-Type', value: 'video/mp4' }];
