06718240b4d1b8834916f04c788335cd6fa54979
Merdi-kim <merdikimuha1@gmail.com>
2023-07-05 14:37:57 +0300

    add video card

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -3,13 +3,24 @@ import HeroBanner from '@/components/HeroBanner';
 import LiveSession from '@/components/LiveSession';
 import Navbar from '@/components/Navbar';
 import LoginModal from '@/components/modals/LoginModal';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import db from '@/lib/weaveDB'
 
 import PopularCategoriesFeed from '@/components/PopularCategoriesFeed';
 import Sidebar from '@/components/SideBar';
+import ReelCard from '@/components/ui/ReelCard';
 
 const Home = () => {
 	const [closeModal, setCloseModal] = useState(true);
+	const [reels, setReels] = useState()
+
+	useEffect(() => {
+		const fetchReels = async() => {
+			const data = await db.get("reels", ["__id__", "desc"])
+			setReels(data)
+		}
+		fetchReels()
+	},[])
 	return (
 		<div className="min-h-full w-full flex">
 			<div className="w-3/12">
@@ -19,9 +30,11 @@ const Home = () => {
 				<Navbar closeModal={setCloseModal} />
 				<HeroBanner />
 				<div className="px-10">
-					{/** live sessions list */}
 					<LiveSession />
 					<PopularCategoriesFeed />
+				</div>
+				<div className='flex flex-wrap'>
+					{ reels?map((reel, index) => <ReelCard reel={reel}/>)}
 				</div>
 			</div>
 			{!closeModal && <LoginModal closeModal={setCloseModal} />}
