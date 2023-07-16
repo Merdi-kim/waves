3c0cdded3426eccbe991e479f9410e959acd91af
Merdi-kim <merdikimuha1@gmail.com>
2023-07-17 01:30:47 +0200

    adjust color pattern on lens profiles

--- src/components/ui/LensProfile.tsx
+++ src/components/ui/LensProfile.tsx
@@ -9,8 +9,8 @@ const LensProfile = ({ profile }: { profile: any }) => {
 			onClick={() => setProfileData(profile)}
 			className={`flex-none flex items-center justify-center h-8 min-w-[150px] px-2 m-4 rounded-lg ${
 				profile.handle == profileData?.handle
-					? 'bg-slate-900 border-[1px] border-white'
-					: 'bg-slate-700'
+					? 'bg-neutral-600 border-[1px] border-white'
+					: 'bg-neutral-700'
 			} text-white hover:text-slate-400 cursor-pointer`}
 		>
 			{profile.handle}
