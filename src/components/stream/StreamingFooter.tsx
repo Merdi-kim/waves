9d86a1f21edca878b74b5578e165b3c64285f497
Jonath-z <jonathanzihindula95@gmail.com>
2023-07-17 23:02:02 +0200

    style: add responsivity

--- src/components/stream/StreamingFooter.tsx
+++ src/components/stream/StreamingFooter.tsx
@@ -13,15 +13,15 @@ const StreamingFooter = () => {
 	const router = useRouter();
 
 	return (
-		<div className="text-white fixed bottom-0 w-full flex justify-between items-center px-10 pb-10">
-			<div className="flex items-center gap-5">
+		<div className="text-white fixed bottom-0 w-full flex justify-between items-center px-4 md:px-10 pb-5 md:pb-10">
+			<div className="flex md:flex-row flex-col items-center gap-5">
 				<PiMicrophoneSlash className="w-6 h-6 cursor-pointer" />
 				<PiVideoCameraLight className="w-6 h-6 cursor-pointer" />
 			</div>
 			<div className="flex items-center gap-5">
-				<PiUploadSimpleBold className="w-6 h-6 cursor-pointer" />
-				<BsThreeDotsVertical className="w-6 h-6 cursor-pointer" />
-				<LuCopy className="w-6 h-6" />
+				<PiUploadSimpleBold className="w-6 h-6 cursor-pointer hidden md:block" />
+				<BsThreeDotsVertical className="w-6 h-6 cursor-pointer hidden md:block" />
+				<LuCopy className="w-6 h-6 hidden md:block" />
 				<div
 					onClick={() => router.replace('/home')}
 					className="flex items-center gap-2 bg-primary px-3 py-2 rounded-lg cursor-pointer"
@@ -31,7 +31,7 @@ const StreamingFooter = () => {
 				</div>
 			</div>
 			<div className="flex items-center gap-5">
-				<HiOutlineHandRaised className="w-6 h-6 cursor-pointer" />
+				<HiOutlineHandRaised className="w-6 h-6 cursor-pointer hidden md:block" />
 				<BsChatLeft className="w-6 h-6 cursor-pointer" />
 			</div>
 		</div>
